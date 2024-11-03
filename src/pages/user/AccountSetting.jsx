1e995f9f836feaabcf2a4aa2f35a6cd7dbb2e7bc
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-11-04 00:21:35 +0500

    routing fixed

--- src/pages/user/AccountSetting.jsx
+++ src/pages/user/AccountSetting.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AccountSetting = () => {
   return (
     <div>
-      AccountSetting
+      <Outlet/>
     </div>
   )
 }
